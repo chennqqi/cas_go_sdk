f49bfab57ff0042cca1e0f2942cd67293cb72033
CHENQ <chennqqi@qq.com>
2019-03-18 21:46:49 +0800

    enhance error print

--- cmd/cascmd/create_vault.go
+++ cmd/cascmd/create_vault.go
@@ -1,11 +1,11 @@
 // Copyright 2019 chennqqi
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -56,7 +56,11 @@ func (p *createVaultCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...inte
 
 	resp, err := vault.CreateVault(ctx, conf.AppId, p.vaultName)
 	if err != nil {
-		fmt.Println("ERROR:", err)
+		if goe, ok := err.(openapi.GenericOpenAPIError); ok {
+			fmt.Println("ERROR:", goe.Model())
+		} else {
+			fmt.Println("ERROR:", err)
+		}
 		return subcommands.ExitFailure
 	}
 	location := resp.Header.Get("Location")
